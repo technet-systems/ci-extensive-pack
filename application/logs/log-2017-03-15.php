fe2e512d151455e668a5c593938104c3f917f453
technet-systems <wojcicki@technet.systems>
2017-03-15 13:50:59 +0100

    added TWIG lib

--- application/logs/log-2017-03-15.php
+++ application/logs/log-2017-03-15.php
@@ -105,3 +105,157 @@ INFO - 2017-03-15 13:07:44 --> Language file loaded: language/polish/form_valida
 INFO - 2017-03-15 13:07:44 --> File loaded: D:\xampp\htdocs\ci-extensive-pack\application\views\welcome_message.php
 INFO - 2017-03-15 13:07:44 --> Final output sent to browser
 DEBUG - 2017-03-15 13:07:44 --> Total execution time: 0.0355
+INFO - 2017-03-15 13:38:58 --> Config Class Initialized
+INFO - 2017-03-15 13:38:58 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:38:58 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:38:58 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:38:58 --> URI Class Initialized
+INFO - 2017-03-15 13:38:58 --> Router Class Initialized
+INFO - 2017-03-15 13:38:58 --> Output Class Initialized
+INFO - 2017-03-15 13:38:58 --> Security Class Initialized
+DEBUG - 2017-03-15 13:38:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:38:58 --> Input Class Initialized
+INFO - 2017-03-15 13:38:58 --> Language Class Initialized
+INFO - 2017-03-15 13:38:58 --> Loader Class Initialized
+INFO - 2017-03-15 13:38:58 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:38:58 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:38:58 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:38:58 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:38:58 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:38:58 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:38:58 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:40:10 --> Config Class Initialized
+INFO - 2017-03-15 13:40:10 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:40:10 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:40:10 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:40:10 --> URI Class Initialized
+INFO - 2017-03-15 13:40:10 --> Router Class Initialized
+INFO - 2017-03-15 13:40:10 --> Output Class Initialized
+INFO - 2017-03-15 13:40:10 --> Security Class Initialized
+DEBUG - 2017-03-15 13:40:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:40:10 --> Input Class Initialized
+INFO - 2017-03-15 13:40:10 --> Language Class Initialized
+INFO - 2017-03-15 13:40:10 --> Loader Class Initialized
+INFO - 2017-03-15 13:40:10 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:40:10 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:40:10 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:40:10 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:40:10 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:40:10 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:40:10 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:40:11 --> Config Class Initialized
+INFO - 2017-03-15 13:40:11 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:40:11 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:40:11 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:40:11 --> URI Class Initialized
+INFO - 2017-03-15 13:40:11 --> Router Class Initialized
+INFO - 2017-03-15 13:40:11 --> Output Class Initialized
+INFO - 2017-03-15 13:40:11 --> Security Class Initialized
+DEBUG - 2017-03-15 13:40:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:40:11 --> Input Class Initialized
+INFO - 2017-03-15 13:40:11 --> Language Class Initialized
+INFO - 2017-03-15 13:40:11 --> Loader Class Initialized
+INFO - 2017-03-15 13:40:11 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:40:11 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:40:11 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:40:11 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:40:11 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:40:11 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:40:11 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:40:12 --> Config Class Initialized
+INFO - 2017-03-15 13:40:12 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:40:12 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:40:12 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:40:12 --> URI Class Initialized
+INFO - 2017-03-15 13:40:12 --> Router Class Initialized
+INFO - 2017-03-15 13:40:12 --> Output Class Initialized
+INFO - 2017-03-15 13:40:12 --> Security Class Initialized
+DEBUG - 2017-03-15 13:40:12 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:40:12 --> Input Class Initialized
+INFO - 2017-03-15 13:40:12 --> Language Class Initialized
+INFO - 2017-03-15 13:40:12 --> Loader Class Initialized
+INFO - 2017-03-15 13:40:12 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:40:12 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:40:12 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:40:12 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:40:12 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:40:12 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:40:12 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:41:13 --> Config Class Initialized
+INFO - 2017-03-15 13:41:13 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:41:13 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:41:13 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:41:13 --> URI Class Initialized
+INFO - 2017-03-15 13:41:13 --> Router Class Initialized
+INFO - 2017-03-15 13:41:13 --> Output Class Initialized
+INFO - 2017-03-15 13:41:13 --> Security Class Initialized
+DEBUG - 2017-03-15 13:41:13 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:41:13 --> Input Class Initialized
+INFO - 2017-03-15 13:41:13 --> Language Class Initialized
+INFO - 2017-03-15 13:41:13 --> Loader Class Initialized
+INFO - 2017-03-15 13:41:13 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:41:13 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:41:13 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:41:13 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:41:13 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:41:13 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:41:13 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:41:14 --> Config Class Initialized
+INFO - 2017-03-15 13:41:14 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:41:14 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:41:14 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:41:14 --> URI Class Initialized
+INFO - 2017-03-15 13:41:14 --> Router Class Initialized
+INFO - 2017-03-15 13:41:14 --> Output Class Initialized
+INFO - 2017-03-15 13:41:14 --> Security Class Initialized
+DEBUG - 2017-03-15 13:41:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:41:14 --> Input Class Initialized
+INFO - 2017-03-15 13:41:14 --> Language Class Initialized
+INFO - 2017-03-15 13:41:14 --> Loader Class Initialized
+INFO - 2017-03-15 13:41:14 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:41:14 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:41:14 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:41:14 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:41:14 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:41:14 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:41:14 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:41:15 --> Config Class Initialized
+INFO - 2017-03-15 13:41:15 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:41:15 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:41:15 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:41:15 --> URI Class Initialized
+INFO - 2017-03-15 13:41:15 --> Router Class Initialized
+INFO - 2017-03-15 13:41:15 --> Output Class Initialized
+INFO - 2017-03-15 13:41:15 --> Security Class Initialized
+DEBUG - 2017-03-15 13:41:15 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:41:15 --> Input Class Initialized
+INFO - 2017-03-15 13:41:15 --> Language Class Initialized
+INFO - 2017-03-15 13:41:15 --> Loader Class Initialized
+INFO - 2017-03-15 13:41:15 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:41:15 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:41:15 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:41:15 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:41:15 --> Session: Class initialized using 'database' driver.
+ERROR - 2017-03-15 13:41:15 --> Severity: Warning --> require_once(Autoloader.php): failed to open stream: No such file or directory D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+ERROR - 2017-03-15 13:41:15 --> Severity: Compile Error --> require_once(): Failed opening required 'Autoloader.php' (include_path='D:\xampp\php\PEAR;D:\xampp\htdocs\ci-extensive-pack\application\third_party/Twig/lib/Twig') D:\xampp\htdocs\ci-extensive-pack\application\libraries\Twig.php 35
+INFO - 2017-03-15 13:46:29 --> Config Class Initialized
+INFO - 2017-03-15 13:46:29 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:46:29 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:46:29 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:46:29 --> URI Class Initialized
+INFO - 2017-03-15 13:46:29 --> Router Class Initialized
+INFO - 2017-03-15 13:46:29 --> Output Class Initialized
+INFO - 2017-03-15 13:46:29 --> Security Class Initialized
+DEBUG - 2017-03-15 13:46:29 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:46:29 --> Input Class Initialized
+INFO - 2017-03-15 13:46:29 --> Language Class Initialized
+INFO - 2017-03-15 13:46:29 --> Loader Class Initialized
+INFO - 2017-03-15 13:46:29 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:46:29 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:46:29 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:46:29 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:46:29 --> Session: Class initialized using 'database' driver.
+DEBUG - 2017-03-15 13:46:29 --> Config file loaded: D:\xampp\htdocs\ci-extensive-pack\application\config/twig.php
+INFO - 2017-03-15 13:46:29 --> Controller Class Initialized
+INFO - 2017-03-15 13:46:29 --> Final output sent to browser
+DEBUG - 2017-03-15 13:46:29 --> Total execution time: 0.0562
