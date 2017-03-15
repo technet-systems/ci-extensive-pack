dcab38ccb557f0df946b1ef101a9620270f6d6aa
technet-systems <wojcicki@technet.systems>
2017-03-15 13:15:21 +0100

    MY_Model and sessions handling configuration

--- application/config/config.php
+++ application/config/config.php
@@ -223,7 +223,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '<5H^}y|vu_.}.fEEz5L2yz4:paZ3:G';
 
 /*
 |--------------------------------------------------------------------------
@@ -377,10 +377,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'cisession';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
---
+++ application/logs/log-2017-03-15.php
@@ -0,0 +1,107 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-03-15 13:06:09 --> Config Class Initialized
+INFO - 2017-03-15 13:06:09 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:06:09 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:06:09 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:06:09 --> URI Class Initialized
+INFO - 2017-03-15 13:06:09 --> Router Class Initialized
+INFO - 2017-03-15 13:06:09 --> Output Class Initialized
+INFO - 2017-03-15 13:06:09 --> Security Class Initialized
+DEBUG - 2017-03-15 13:06:09 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:06:09 --> Input Class Initialized
+INFO - 2017-03-15 13:06:09 --> Language Class Initialized
+INFO - 2017-03-15 13:06:09 --> Loader Class Initialized
+INFO - 2017-03-15 13:06:09 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:06:09 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:06:09 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:06:09 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:06:09 --> Session: Class initialized using 'database' driver.
+INFO - 2017-03-15 13:06:09 --> Controller Class Initialized
+INFO - 2017-03-15 13:06:09 --> Model Class Initialized
+INFO - 2017-03-15 13:06:09 --> Helper loaded: inflector_helper
+INFO - 2017-03-15 13:06:09 --> Model Class Initialized
+INFO - 2017-03-15 13:06:09 --> Model Class Initialized
+INFO - 2017-03-15 13:06:09 --> Model Class Initialized
+INFO - 2017-03-15 13:06:09 --> Helper loaded: file_helper
+DEBUG - 2017-03-15 13:06:09 --> Cache class already loaded. Second attempt ignored.
+INFO - 2017-03-15 13:06:09 --> File loaded: D:\xampp\htdocs\ci-extensive-pack\application\views\test_view.php
+INFO - 2017-03-15 13:06:09 --> Language file loaded: language/polish/profiler_lang.php
+INFO - 2017-03-15 13:06:09 --> Profiler Class Initialized
+INFO - 2017-03-15 13:06:09 --> Helper loaded: text_helper
+INFO - 2017-03-15 13:06:09 --> Final output sent to browser
+DEBUG - 2017-03-15 13:06:09 --> Total execution time: 0.0518
+INFO - 2017-03-15 13:07:12 --> Config Class Initialized
+INFO - 2017-03-15 13:07:12 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:07:12 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:07:12 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:07:12 --> URI Class Initialized
+INFO - 2017-03-15 13:07:12 --> Router Class Initialized
+INFO - 2017-03-15 13:07:12 --> Output Class Initialized
+INFO - 2017-03-15 13:07:12 --> Security Class Initialized
+DEBUG - 2017-03-15 13:07:12 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:07:12 --> Input Class Initialized
+INFO - 2017-03-15 13:07:12 --> Language Class Initialized
+INFO - 2017-03-15 13:07:12 --> Loader Class Initialized
+INFO - 2017-03-15 13:07:12 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:07:12 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:07:12 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:07:12 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:07:12 --> Session: Class initialized using 'database' driver.
+INFO - 2017-03-15 13:07:12 --> Controller Class Initialized
+INFO - 2017-03-15 13:07:12 --> Model Class Initialized
+INFO - 2017-03-15 13:07:12 --> Helper loaded: inflector_helper
+INFO - 2017-03-15 13:07:12 --> Model Class Initialized
+INFO - 2017-03-15 13:07:12 --> Model Class Initialized
+INFO - 2017-03-15 13:07:12 --> Model Class Initialized
+INFO - 2017-03-15 13:07:12 --> Helper loaded: file_helper
+DEBUG - 2017-03-15 13:07:12 --> Cache class already loaded. Second attempt ignored.
+INFO - 2017-03-15 13:07:12 --> File loaded: D:\xampp\htdocs\ci-extensive-pack\application\views\test_view.php
+INFO - 2017-03-15 13:07:12 --> Language file loaded: language/polish/profiler_lang.php
+INFO - 2017-03-15 13:07:12 --> Profiler Class Initialized
+INFO - 2017-03-15 13:07:12 --> Helper loaded: text_helper
+INFO - 2017-03-15 13:07:12 --> Final output sent to browser
+DEBUG - 2017-03-15 13:07:12 --> Total execution time: 0.0531
+INFO - 2017-03-15 13:07:42 --> Config Class Initialized
+INFO - 2017-03-15 13:07:42 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:07:42 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:07:42 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:07:42 --> URI Class Initialized
+INFO - 2017-03-15 13:07:42 --> Router Class Initialized
+INFO - 2017-03-15 13:07:42 --> Output Class Initialized
+INFO - 2017-03-15 13:07:42 --> Security Class Initialized
+DEBUG - 2017-03-15 13:07:42 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:07:42 --> Input Class Initialized
+INFO - 2017-03-15 13:07:42 --> Language Class Initialized
+INFO - 2017-03-15 13:07:42 --> Loader Class Initialized
+INFO - 2017-03-15 13:07:42 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:07:42 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:07:42 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:07:42 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:07:42 --> Session: Class initialized using 'database' driver.
+INFO - 2017-03-15 13:07:42 --> Controller Class Initialized
+INFO - 2017-03-15 13:07:42 --> File loaded: D:\xampp\htdocs\ci-extensive-pack\application\views\welcome_message.php
+INFO - 2017-03-15 13:07:42 --> Final output sent to browser
+DEBUG - 2017-03-15 13:07:42 --> Total execution time: 0.0349
+INFO - 2017-03-15 13:07:44 --> Config Class Initialized
+INFO - 2017-03-15 13:07:44 --> Hooks Class Initialized
+DEBUG - 2017-03-15 13:07:44 --> UTF-8 Support Enabled
+INFO - 2017-03-15 13:07:44 --> Utf8 Class Initialized
+INFO - 2017-03-15 13:07:44 --> URI Class Initialized
+INFO - 2017-03-15 13:07:44 --> Router Class Initialized
+INFO - 2017-03-15 13:07:44 --> Output Class Initialized
+INFO - 2017-03-15 13:07:44 --> Security Class Initialized
+DEBUG - 2017-03-15 13:07:44 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-15 13:07:44 --> Input Class Initialized
+INFO - 2017-03-15 13:07:44 --> Language Class Initialized
+INFO - 2017-03-15 13:07:44 --> Loader Class Initialized
+INFO - 2017-03-15 13:07:44 --> Helper loaded: form_helper
+INFO - 2017-03-15 13:07:44 --> Helper loaded: url_helper
+INFO - 2017-03-15 13:07:44 --> Form Validation Class Initialized
+INFO - 2017-03-15 13:07:44 --> Database Driver Class Initialized
+INFO - 2017-03-15 13:07:44 --> Session: Class initialized using 'database' driver.
+INFO - 2017-03-15 13:07:44 --> Controller Class Initialized
+INFO - 2017-03-15 13:07:44 --> Language file loaded: language/polish/form_validation_lang.php
+INFO - 2017-03-15 13:07:44 --> File loaded: D:\xampp\htdocs\ci-extensive-pack\application\views\welcome_message.php
+INFO - 2017-03-15 13:07:44 --> Final output sent to browser
+DEBUG - 2017-03-15 13:07:44 --> Total execution time: 0.0355
